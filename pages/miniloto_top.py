0b6afc99f3989cda882bed113e030336033319c2
Naobro <naokiwm@icloud.com>
2026-02-19 18:52:33 +0900

    Remove password check from miniloto_top.py

--- pages/miniloto_top.py
+++ pages/miniloto_top.py
@@ -5,7 +5,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import streamlit as st
 from auth import check_password
 
-check_password()
 st.set_page_config(layout="centered")
 
 import ssl
